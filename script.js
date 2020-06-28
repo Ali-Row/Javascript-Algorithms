b6d1954eccb7328236825fcc27012c3bd0427440
Alistair <deadlyhardware@gmail.com>
2020-06-27 23:54:59 -0400

    Add arrToString function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// These functions below will utilize the newer ES6 syntax and methods!
 
 // This function sorts an array of numbers from lowest to highest
 let sortArr = (arr) => {
@@ -7,3 +8,17 @@ let sortArr = (arr) => {
 // Calling the sortArr function and passing in a randomized array of numbers 
 sortArr([1, 55, 3000, 22, 5, 34, 56, 1000, 343]);
 
+
+// This function converts an array of strings into a single string
+let arrToString = (arr) => {
+    str = arr.join(' ');
+    console.log('arrToString function output:', str);
+}
+// Calling the arrToString function and passing in an array with 2 strings inside
+arrToString(['Hello', 'World!']);
+
+
+// This function will count the number of vowels in a string
+let countVowels = (str) => {
+    let vowels;
+}
