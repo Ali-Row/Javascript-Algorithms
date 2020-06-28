f3992e7fd2705780ab589a311d178eb9a0a761f5
Alistair <deadlyhardware@gmail.com>
2020-06-27 23:35:37 -0400

    Add sort array algorithm

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// This function sorts an array of numbers from lowest to highest
+let sortArr = (arr) => {
+    arr.sort((a, b) => a-b);
+    console.log('sortArr function output:', arr);
+}
+
+// Calling the sortArr function and passing in a randomized array of numbers 
+sortArr([1, 55, 3000, 22, 5, 34, 56, 1000, 343])
